caa0ae51998aafda0a3640156dfd3913a1ffcccc
paul <zedozon@gmail.com>
2022-03-29 18:29:02 +0800

    feat: parallax effect

--- lib/pages/home_page/index.dart
+++ lib/pages/home_page/index.dart
@@ -6,36 +6,75 @@ import 'package:mighty_book/pages/home_page/widgets/book_item.dart';
 import 'package:mighty_book/routes/app_pages.dart';
 
 class HomePage extends StatelessWidget {
-  HomePage({Key? key}) : super(key: key);
-
-  final BookController _controller = BookController.to;
+  const HomePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
-      body: Container(
-        child: Obx(() {
-          return Column(
-            children: _controller.list
-                .map(
-                  (el) => InkWell(
-                    onTap: () {
-                      Get.toNamed(ROUTES.book, arguments: el);
-                    },
-                    child: BookItem(el),
-                  ),
-                )
-                .toList(),
-          );
-        }),
-      ),
+      body: const SlidingCardsView(),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
           Get.toNamed(ROUTES.addBook);
         },
-        child: Icon(Icons.add),
+        child: const Icon(Icons.add),
       ),
     );
+  }
+}
+
+class SlidingCardsView extends StatefulWidget {
+  const SlidingCardsView({Key? key}) : super(key: key);
+
+  @override
+  State<SlidingCardsView> createState() => _SlidingCardsViewState();
+}
+
+class _SlidingCardsViewState extends State<SlidingCardsView> {
+  late PageController pageController;
+  double? pageOffset = 0;
+  final BookController _controller = BookController.to;
+
+  @override
+  void initState() {
+    pageController = PageController(viewportFraction: 0.8);
+    pageController.addListener(() {
+      setState(() {
+        pageOffset = pageController.page;
+      });
+    });
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    pageController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+        height: Get.height * 0.55,
+        child: Obx(() {
+          int index = -1;
+          return PageView(
+            controller: pageController,
+            children: _controller.list.map((l) {
+              index++;
+              return InkWell(
+                onTap: () {
+                  Get.toNamed(ROUTES.book, arguments: l);
+                },
+                child: BookItem(
+                  name: l.name,
+                  date: l.startAt.toString(),
+                  assetName: 'assets/images/lulu_2.png',
+                  offset: (pageOffset ?? 0) - index,
+                ),
+              );
+            }).toList(),
+          );
+        }));
   }
 }
--- lib/pages/home_page/widgets/book_item.dart
+++ lib/pages/home_page/widgets/book_item.dart
@@ -1,28 +1,88 @@
 import 'package:flutter/material.dart';
-import 'package:mighty_book/models/book_model.dart';
+import 'package:get/route_manager.dart';
+import 'dart:math' as math;
 
 class BookItem extends StatelessWidget {
-  const BookItem(this._book, { Key? key }) : super(key: key);
-  final Book _book;
-
+  const BookItem({
+    required this.name,
+    required this.date,
+    required this.assetName,
+    required this.offset,
+    Key? key,
+  }) : super(key: key);
+  final String name;
+  final String date;
+  final String assetName;
+  final double offset;
 
   @override
   Widget build(BuildContext context) {
-    return Container(
+    double gauss = math.exp(-(math.pow((offset.abs() - 0.5), 2) /
+        0.08)); //<--caluclate Gaussian function
+
+    return Card(
+      margin: const EdgeInsets.only(left: 8, right: 8, bottom: 24),
+      elevation: 8,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(32),
+      ),
+      child: Transform.translate(
+        offset: Offset(-32 * gauss * offset.sign, 0),
+        child: Column(
+          children: [
+            ClipRRect(
+              borderRadius: const BorderRadius.vertical(
+                top: Radius.circular(32),
+              ),
+              child: Image.asset(
+                assetName,
+                fit: BoxFit.none,
+                height: Get.height * 0.4,
+                width: double.infinity,
+                alignment: Alignment(-offset.abs(), 0), //<-- Set the alignment
+              ),
+            ),
+            const SizedBox(
+              height: 8,
+            ),
+            Expanded(
+                child: CardContent(
+              name: name,
+              date: date,
+              offset: offset,
+            ))
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class CardContent extends StatelessWidget {
+  final String name;
+  final String date;
+  final double offset;
+
+  const CardContent({
+    Key? key,
+    required this.name,
+    required this.date,
+    required this.offset,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Text(_book.name, style: Theme.of(context).textTheme.headline3,),
-          Text('${_book.startAt.year}-${_book.startAt.month}-${_book.startAt.day}', style: Theme.of(context).textTheme.subtitle1)
+        children: <Widget>[
+          Transform.translate(
+              offset: Offset(8 * offset, 0),
+              child: Text(name, style: const TextStyle(fontSize: 20))),
+          const SizedBox(height: 8),
+          Text(date, style: const TextStyle(color: Colors.grey)),
         ],
-      ),
-      width: double.infinity,
-      height: 100,
-      padding: const EdgeInsets.all(10),
-      margin: const EdgeInsets.all(16),
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(10),
-        color: Theme.of(context).primaryColor,
       ),
     );
   }
